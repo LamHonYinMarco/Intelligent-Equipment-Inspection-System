54a311632a11d5239a8c732b5da66a6722fc7a08
LAPTOP-O4G0M3OS\user <marcolam622@gmail.com>
2021-12-25 02:24:36 +0800

    Navigation bar and UI

--- app/src/main/java/com/example/intelligentequipmentinspectionsystem/MainActivity.java
+++ app/src/main/java/com/example/intelligentequipmentinspectionsystem/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.intelligentequipmentinspectionsystem;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+       
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+
     }
+
 }
